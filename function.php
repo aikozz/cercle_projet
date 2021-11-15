9aad7b2d9aa1ccc713327dc45aeeb305f0e8778a
nathan <pellissiere.nathan.jrd@gmail.com>
2021-11-15 17:30:31 +0100

    ajout des fonctions

---
+++ function.php
@@ -0,0 +1,29 @@
+<?php
+function getLastKeyword(){
+    require('db.php');
+    
+    $req = $connexion->prepare("SELECT * FROM log JOIN mot-cle ON mot-cle.id = log.id-mot-cle LIMIT 1 ORDER BY DESC");
+    $req = execute();
+    $res = $req->fetch();
+    return $res['libelle'];
+}
+
+function getLastResa(){
+    require('db.php');
+    
+    $req = $connexion->prepare("SELECT * FROM reservation LIMIT 1 ORDER BY DESC");
+    $req = execute();
+    $res = $req->fetch();
+    return $res;
+
+}
+
+function getCountKeyword(){
+    require('db.php');
+    
+      
+    $req = $connexion->prepare("SELECT COUNT(*) FROM log JOIN mot-cle ON mot-cle.id = log.id-mot-cle LIMIT 1 ORDER BY DESC");
+    $req = execute();
+    $res = $req->fetch();
+    return $res;
+}
