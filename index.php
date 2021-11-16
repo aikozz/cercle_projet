55e4f16594a5104963ef2c65dcd7ce29338acc4f
nathan <pellissiere.nathan.jrd@gmail.com>
2021-11-16 09:50:20 +0100

    delete =

--- index.php
+++ index.php
@@ -33,11 +33,6 @@
 			<div class="navbar-header">
 				<a class="navbar-brand" href="index.html"><img src="assets/images/logo.png" alt="Progressus HTML5 template"></a>
 			</div>
-<<<<<<< HEAD
-			
-			</div><!--/.nav-collapse -->
-=======
->>>>>>> 184633313fa938ffb7086f3819c0b5a451daae1c
 		</div>
 	</div>
 	<!-- /.navbar -->
@@ -52,21 +47,6 @@
 		</div>
 	</header>
 	<!-- /Header -->
-<<<<<<< HEAD
-	<!-- Intro -->
-	<div class="container text-center">
-		<br> <br>
-		<h2 class="thin">The best place to tell people why they are here</h2>
-		<p class="text-muted">
-			The difference between involvement and commitment is like an eggs-and-ham breakfast:<br> 
-			the chicken was involved; the pig was committed.
-		</p>
-	</div>
-	<!-- /Intro-->
-		
-=======
-
->>>>>>> 184633313fa938ffb7086f3819c0b5a451daae1c
 	<!-- Highlights - jumbotron -->
 	<div class="jumbotron top-space">
 		<div class="container">
@@ -123,32 +103,11 @@
 
 		<div class="row">
 			<div class="col-sm-6">
-<<<<<<< HEAD
 				<h3>Documentation d'installation</h3>
 				<p>La documentation décrivant étape par étape, de façon concise l'installation dans le but de faciliter la prise en main
 					de l'utilisateur.
 					Une version complète est à retrouvée ci-dessous.
 					<p class="text-center"><a class="btn btn-success btn-large"><i class="fa fa-download" aria-hidden="true"></i> Télécharger</a></p>
-=======
-				<h3>Which code editor would you recommend?</h3>
-				<p>I'd highly recommend you <a href="http://www.sublimetext.com/">Sublime Text</a> - a free to try text editor which I'm using daily. Awesome tool!</p>
-			</div>
-			<div class="col-sm-6">
-				<h3>Nice header. Where do I find more images like that one?</h3>
-				<p>
-					Well, there are thousands of stock art galleries, but personally,
-					I prefer to use photos from these sites: <a href="http://unsplash.com">Unsplash.com</a>
-					and <a href="http://www.flickr.com/creativecommons/by-2.0/tags/">Flickr - Creative Commons</a></p>
-			</div>
-		</div> <!-- /row -->
-
-		<div class="row">
-			<div class="col-sm-6">
-				<h3>Can I use it to build a site for my client?</h3>
-				<p>
-					Yes, you can. You may use this template for any purpose, just don't forget about the <a href="http://creativecommons.org/licenses/by/3.0/">license</a>,
-					which says: "You must give appropriate credit", i.e. you must provide the name of the creator and a link to the original template in your work.
->>>>>>> 184633313fa938ffb7086f3819c0b5a451daae1c
 				</p>
 			</div>
 			<div class="col-sm-6">
@@ -160,28 +119,10 @@
 				</p>
 			</div>
 		</div> <!-- /row -->
-<<<<<<< HEAD
-			<!-- /container -->
-	
-	<!-- Social links. @TODO: replace by link/instructions in template -->
-	<section id="social">
-		<div class="container">
-			<div class="wrapper clearfix">
-				<!-- AddThis Button BEGIN -->
-				<div class="addthis_toolbox addthis_default_style">
-				<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
-				<a class="addthis_button_tweet"></a>
-				<a class="addthis_button_linkedin_counter"></a>
-				<a class="addthis_button_google_plusone" g:plusone:size="medium"></a>
-				</div>
-				<!-- AddThis Button END -->
-			</div>
-=======
 
 		<div class="jumbotron top-space">
 			<h4>Dicta, nostrum nemo soluta sapiente sit dolor quae voluptas quidem doloribus recusandae facere magni ullam suscipit sunt atque rerum eaque iusto facilis esse nam veniam incidunt officia perspiciatis at voluptatibus. Libero, aliquid illum possimus numquam fuga.</h4>
 			<p class="text-right"><a class="btn btn-primary btn-large">Learn more »</a></p>
->>>>>>> 184633313fa938ffb7086f3819c0b5a451daae1c
 		</div>
 	</div> <!-- /container -->
 
@@ -220,12 +161,8 @@
 			</div>
 		</div>
 
-<<<<<<< HEAD
-	</footer>	
-=======
 	</footer>
 
->>>>>>> 184633313fa938ffb7086f3819c0b5a451daae1c
 	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
